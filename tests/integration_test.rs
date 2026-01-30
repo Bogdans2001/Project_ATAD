b9338a6a43b047048db4e6060a85780efdbe27e7
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2026-01-31 01:33:33 +0200

    change app name to finance_app

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 #[test]
 fn test_report_category_correct() {
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["report", "--property", "category"]).output()
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["report", "--property", "category"]).output()
         .expect("Command not executed");
     assert!(output.status.success(),"Command not found. stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
@@ -10,28 +10,28 @@ fn test_report_category_correct() {
 
 #[test]
 fn test_report_category_wrong() {
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["report", "--option", "category"]).output()
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["report", "--option", "category"]).output()
         .expect("Command not executed");
     assert!(!output.status.success() , "The command was invalid");
 }
 
 #[test]
 fn test_report_monthly_correct() {
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["report", "--property", "monthly"]).output()
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["report", "--property", "monthly"]).output()
         .expect("Command not executed");
     assert!(output.status.success(),"Command not found. stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
 
 #[test]
 fn test_report_monthly_wrong() {
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["report", "--option", "monthly"]).output()
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["report", "--option", "monthly"]).output()
         .expect("Command not executed");
     assert!(!output.status.success() , "The command was invalid");
 }
 
 #[test]
 fn test_search_command(){
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["search", "--date", "2003-12-12"]).output()
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["search", "--date", "2003-12-12"]).output()
         .expect("Command not executed");
     assert!(output.status.success(),"Command not found. stderr: {}", String::from_utf8_lossy(&output.stderr));
     
@@ -41,7 +41,7 @@ fn test_search_command(){
 
 #[test]
 fn test_search_command_wrong() {
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["search", "--date", "2003-13-13"]).output()
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["search", "--date", "2003-13-13"]).output()
         .expect("Command not executed");
     
     assert!(!output.status.success() , "The date was invalid");
@@ -49,7 +49,7 @@ fn test_search_command_wrong() {
 
 #[test]
 fn test_add_command_wrong() {
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["add", "--kind", "expense", "--amount",
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["add", "--kind", "expense", "--amount",
         "18", "--category-id", "0", "--date", "2025-09-33", "--description", "diesel"]).output()
         .expect("Command not executed");
     
@@ -58,7 +58,7 @@ fn test_add_command_wrong() {
 
 #[test]
 fn test_budget_command(){
-    let output = Command::new(env!("CARGO_BIN_EXE_personal_finance_cli_manager")).args(["--category-id", "54", "--month", "2025-09", 
+    let output = Command::new(env!("CARGO_BIN_EXE_finance_app")).args(["--category-id", "54", "--month", "2025-09", 
         "--amount", "35"]).output().expect("Command not executed");
     assert!(!output.status.success() , "The category id was invalid");
 }
